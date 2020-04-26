7302e304ecee25bdb15ddbf55cb789a27b844003
izmir <izmir@192.168.1.27>
2020-04-26 16:48:31 +0300

    Silme ve uniq işleminden önceki hal

--- src/rapor/CalisanEkleController.java
+++ src/rapor/CalisanEkleController.java
@@ -19,7 +19,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
@@ -28,7 +27,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.util.converter.LocalDateStringConverter;
 
